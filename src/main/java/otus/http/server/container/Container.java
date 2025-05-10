a8291e3d2a7e7907bbdee9c31df53e849826072b
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-05-10 17:20:14 +0300

    prepare servlet loader

--- src/main/java/otus/http/server/container/Container.java
+++ src/main/java/otus/http/server/container/Container.java
@@ -3,13 +3,17 @@ package otus.http.server.container;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Container {
     private final Logger logger = LogManager.getLogger(this.getClass().getName());
     private final FileListener fileListener;
+    private final ConcurrentHashMap<Path, HttpServlet> servlets;
 
     public Container() {
         final var path = Paths.get("./containers");
@@ -20,11 +24,29 @@ public class Container {
                 throw new RuntimeException(e);
             }
         }
+        servlets = new ConcurrentHashMap<>();
         fileListener = new FileListener(path);
         fileListener.setOnCreate((into) -> {
+            if(Files.isRegularFile(into) && into.endsWith(".war")){
+                logger.info("Tar war with name: {}", into);
+
+                return;
+            }
+            if(servlets.contains(into)){
+                logger.info("current servlet {} exists", into);
+                return;
+            }
+            final var servletLoader = new ServletLoader(into);
+            final var httpServlet = servletLoader.load();
+            servlets.put(path, httpServlet);
             logger.info("on create {}", into);
         });
         fileListener.setOnDelete((into) -> {
+            if(!servlets.contains(into)){
+               return;
+            }
+            //todo stop servlet
+            final var httpServlet = servlets.remove(into);
             logger.info("on delete {}", into);
         });
     }
---
+++ src/main/java/otus/http/server/container/ServletLoader.java
@@ -0,0 +1,16 @@
+package otus.http.server.container;
+
+import javax.servlet.http.HttpServlet;
+import java.nio.file.Path;
+
+public class ServletLoader {
+    private final Path path;
+
+    public ServletLoader(Path path){
+        this.path = path;
+    }
+
+    public HttpServlet load(){
+
+    }
+}
